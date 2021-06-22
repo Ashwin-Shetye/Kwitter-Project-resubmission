dc9ebea56c030d09ba6898642fb95c956276eab3
Ashwin-Shetye <80038082+Ashwin-Shetye@users.noreply.github.com>
2021-06-22 21:52:35 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCymKApznKGCXfb_bJSh0J8358ALbWMT3Y",
+    authDomain: "kwitter-project-74bea.firebaseapp.com",
+    databaseURL: "https://kwitter-project-74bea-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-74bea",
+    storageBucket: "kwitter-project-74bea.appspot.com",
+    messagingSenderId: "814671804466",
+    appId: "1:814671804466:web:473eb4fe6338596a4ce42b",
+    measurementId: "G-M0ZNESHK3T"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
